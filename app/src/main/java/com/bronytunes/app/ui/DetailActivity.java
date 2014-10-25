9b9f9c82fead5e2271f5976d1446359374c7e70b
berwyn <berwyn.codeweaver@gmail.com>
2014-10-25 03:59:44 -0600

    There's paper, there's ink, and there's a very drunk me

--- app/src/main/java/com/bronytunes/app/ui/DetailActivity.java
+++ app/src/main/java/com/bronytunes/app/ui/DetailActivity.java
@@ -1,23 +1,68 @@
 package com.bronytunes.app.ui;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import com.bronytunes.API;
 import com.bronytunes.app.R;
 
-public class DetailActivity extends Activity {
+import javax.inject.Inject;
 
-    TextView tv;
+public class DetailActivity extends ActionBarActivity {
+
+    API api;
+
+    private Fragment displayFragment;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
 
+        toolbar = ((Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar(toolbar);
+
+        api = new API.Builder().build();
+
+        if (getIntent().getData() == null) {
+            finish();
+        }
+
+        String resource;
+        int id;
+        Uri uri = getIntent().getData();
+        if (uri.getScheme().equals("bronytunes")) {
+            resource = uri.getHost();
+            id = Integer.parseInt(uri.getPath().replaceFirst("/", ""), 10);
+        } else {
+            // The path leads with a '/', so we want to remove that and split
+            // into its individual components
+            String[] parts = uri.getPath().substring(1).split("/");
+            resource = parts[0];
+            // Our path slug is in the format id-name-of-song
+            String[] slugParts = parts[1].split("-");
+            id = Integer.parseInt(slugParts[0], 10);
+        }
+
+        switch(resource) {
+            default:
+            case "songs":
+                displayFragment = SongDetailFragment.newInstance(id);
+                break;
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, displayFragment)
+                .commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
